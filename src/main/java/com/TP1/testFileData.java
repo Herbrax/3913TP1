811770d2dab225bdf741490c00a5edd73baff6df
Herbrax <simo.hakim@umontreal.ca>
2023-10-06 20:28:59 -0400

    compiled all

--- src/main/java/com/TP1/testFileData.java
+++ src/main/java/com/TP1/testFileData.java
@@ -1,15 +1,16 @@
 package com.TP1;
-
 public class testFileData {
-	String filePath;
+    String filePath;
     String packageName;
     String className;
     int tloc;
     int tassert;
     double tcm;
 
+    public testFileData() {
+    }
 
-    testFileData() {
+    public testFileData(String filePath, String packageName, String className, int tloc, int tassert, double tcm) {
         this.filePath = filePath;
         this.packageName = packageName;
         this.className = className;
