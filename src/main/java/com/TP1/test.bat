5c1ab43136ab6225e1fbd96223f5eb684de3fdfe
Herbrax <simo.hakim@umontreal.ca>
2023-10-06 21:51:41 -0400

    Fichier BAT de test

---
+++ src/main/java/com/TP1/test.bat
@@ -0,0 +1,7 @@
+java -jar TASSERTCounter.jar jfreechart-master\src\test\java\org\jfree\data\RangeTest.java
+java -jar TLOCCounter.jar jfreechart-master\src\test\java\org\jfree\data\RangeTest.java
+java -jar TROPCOMP.jar -o "outputTROPCOMP1.csv" "jfreechart-master" 1
+java -jar TROPCOMP.jar -o "outputTROPCOMP5.csv" "jfreechart-master" 5
+java -jar TROPCOMP.jar -o "outputTROPCOMP10.csv" "jfreechart-master" 10
+java -jar TROPCOMP.jar "jfreechart-master" 10
+java -jar TLS.jar "jfreechart-master" "outputTLS.csv"
